371073c56adbc68c9f3bfff91cfad810363ba294
Frederico Liporace <liporace@amskepler.com>
2021-10-28 10:47:07 -0300

    Functions to reload .dir-locals.el on demand

--- init.el
+++ init.el
@@ -444,16 +444,31 @@
 (global-set-key (kbd "C-x M-g") 'magit-dispatch)
 
 ;; hideshow
-;; This is taking too long with EXWM, disabling for now
-;; (defun my-hide-all()
-;;   (interactive)
-;;   (hs-minor-mode)
-;;   (hs-hide-all))
-;; (add-hook 'prog-mode-hook 'my-hide-all)
+(defun my-hide-all()
+  (interactive)
+  (hs-minor-mode)
+  (hs-hide-all))
+(add-hook 'prog-mode-hook 'my-hide-all)
 ;;hiding block of code
 (global-set-key (kbd "C-c h") (kbd "C-c @ C-h"))
 ;;show block of code
 (global-set-key (kbd "C-c s") (kbd "C-c @ C-s"))
+
+;; tools to reload .dir-locals.el from
+;; https://emacs.stackexchange.com/a/13096/31354
+(defun my-reload-dir-locals-for-current-buffer ()
+  "reload dir locals for the current buffer"
+  (interactive)
+  (let ((enable-local-variables :all))
+    (hack-dir-local-variables-non-file-buffer)))
+(defun my-reload-dir-locals-for-all-buffer-in-this-directory ()
+  "For every buffer with the same `default-directory` as the current buffer's, reload dir-locals."
+  (interactive)
+  (let ((dir default-directory))
+    (dolist (buffer (buffer-list))
+      (with-current-buffer buffer
+        (when (equal default-directory dir)
+          (my-reload-dir-locals-for-current-buffer))))))
 
 (provide 'init)
 ;;; init.el ends here
