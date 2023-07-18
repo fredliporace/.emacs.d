129c7a1a1e75ad1aa455dd7e16cf9b2226572214
Frederico Liporace <liporace@amskepler.com>
2023-07-18 17:57:38 -0300

    Javascript configuration

--- init.el
+++ init.el
@@ -68,6 +68,9 @@
     irony
     pytest
     magit
+    js2-mode
+    js2-refactor
+    xref-js2
     use-package))
 
 (mapc #'(lambda (package)
@@ -452,6 +455,19 @@
 
 ;; Pytest
 (use-package pytest)
+
+;; Javascript configuration
+;; https://emacs.cafe/emacs/javascript/setup/2017/04/23/emacs-setup-javascript.html
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-r")
+(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
+;; unbind it.
+(define-key js-mode-map (kbd "M-.") nil)
+(add-hook 'js2-mode-hook (lambda ()
+  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
 
 ;; magit
 (global-set-key (kbd "C-x g") 'magit-status)
